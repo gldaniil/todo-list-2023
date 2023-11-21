a33f9a8f0dbc40fd7af5fc0fe7fa2bf58d3a8c8b
gldaniil <daniil.gajfullin@yandex.ru>
2023-11-21 22:55:05 +0500

    Правки в основных файлах

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import './styles/reset.scss';
 import './App.css';
+import FirstVisit from './components/Loading/FirstVisit';
+import Navigation from './components/UI/Navigation';
+import Todos from './components/Todos/Todos';
 
 const App = () => {
-  return <div className="App"></div>;
+  const checkUser = window.localStorage.getItem('wasHere');
+  checkUser ? 0 : window.localStorage.setItem('wasHere', true);
+
+  return (
+    <div className="App">
+      {!checkUser && <FirstVisit />}
+      <Navigation />
+      <Todos />
+    </div>
+  );
 };
 
 export default App;
