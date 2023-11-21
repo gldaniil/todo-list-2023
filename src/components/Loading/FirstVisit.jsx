3ff64defef0ccdf7327bcc4d1479670a9c6fb0d1
gldaniil <daniil.gajfullin@yandex.ru>
2023-11-21 22:56:24 +0500

    Компонент для первого посещения

---
+++ src/components/Loading/FirstVisit.jsx
@@ -0,0 +1,11 @@
+import styles from './FirstVisit.module.scss';
+
+const FirstVisit = () => {
+  return (
+    <div className={styles.loader}>
+      <h1>Example h1-title</h1>
+    </div>
+  );
+};
+
+export default FirstVisit;
