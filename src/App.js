2cfbe2ce17951126d224edbb93acf99311ace4a5
gldaniil <daniil.gajfullin@yandex.ru>
2023-12-03 20:04:00 +0500

    Убрано уведомление при первом посещении

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import './styles/reset.scss';
 import styles from './App.module.css';
-import FirstVisit from './components/Loading/FirstVisit';
 import Navigation from './components/UI/Navigation';
 import Footer from './components/UI/Footer';
 import Main from './components/UI/Main';
 import { BrowserRouter } from 'react-router-dom';
 
 const App = () => {
-  const checkUser = window.localStorage.getItem('wasHere');
-  checkUser ? 0 : window.localStorage.setItem('wasHere', true);
-
   return (
     <div className={styles.App}>
       <BrowserRouter>
-        {!checkUser && <FirstVisit />}
         <Navigation />
         <Main />
         <Footer />
